56bba980af96ff95e3461c5599ef6c4a0d29e628
UtkarshPrakash <48208181+UtkarshPrakash@users.noreply.github.com>
2019-10-26 12:08:14 +0530

    Update 16123004.py

--- Binary Search/16123004.py
+++ Binary Search/16123004.py
@@ -1,7 +1,22 @@
-l = [ 2,3,4,5,6,8]
-
-n = len(l)
-for i in n:
-	# TODO Fill Code here
+def Binary_search(arr,start_index,last_index,element):
+	while (start_index<= last_index):
+		mid =(int)(start_index+last_index)/2
+		if (element>arr[mid]):
+			start_index = mid+1
+		elif (element<arr[mid]):
+			last_index = mid-1
+		elif (element == arr[mid]):
+			return mid
+	return -1
+	
+l = [2,3,4,5,6,8]
+element = 6
+start_index = 0
+last_index = len(l)-1
+found = Binary_search(arr,start_index,last_index,element)
+if (found == -1):
+	print "element not present in array"
+else:
+	print "element is present at index " + str(found)	
 
 
